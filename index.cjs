d5a02a0c5577500ea6970cbb1428099de8e9fc50
ir30me <lars.vonk@ing.com>
2023-08-03 18:08:05 +0200

    chore: added class naming eslint rule

--- index.cjs
+++ index.cjs
@@ -1,9 +1,11 @@
 const UPPERCASE_LITERAL_CONSTANT = require('./rules/uppercase-literal-constant.cjs');
 const METHOD_NAMING = require('./rules/method-naming.cjs');
+const CLASS_NAMING = require('./rules/class-naming.cjs');
 
 module.exports = {
     rules: {
         'uppercase-literal-constant': UPPERCASE_LITERAL_CONSTANT,
         'method-naming': METHOD_NAMING,
+        'class-naming': CLASS_NAMING
     },
 };
---
+++ rules/class-naming.js
@@ -0,0 +1,30 @@
+export const CLASS_NAMING = {
+	meta: {
+		type: 'Problem',
+		docs: {
+			description: 'Classes should be PascalCase!',
+		},
+		messages: {
+			pascalcase: "'{{ name }}': Class name should be PascalCase",
+		},
+	},
+	create: context => ({
+		ClassDeclaration(node) {
+			if (node.id.type === 'Identifier') {
+				if (
+					node.id.name !== node.id.name.charAt(0).toUpperCase() &&
+					node.id.name.length > 1 &&
+					!node.id.name.includes('_')
+				) {
+					context.report({
+						node: node.id,
+						data: {
+							name: node.id.name,
+						},
+						messageId: 'pascalcase',
+					});
+				}
+			}
+		},
+	}),
+};
