646fc4e25d1eaa3a063340d0e9572ed1733017e8
ir30me <lars.vonk@ing.com>
2023-08-03 18:08:05 +0200

    chore: added class naming eslint rule

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import { UPPERCASE_LITERAL_CONSTANT } from "./rules/uppercase-literal-constant";
 import { METHOD_NAMING } from "./rules/method-naming";
+import { CLASS_NAMING } from "./rules/class-naming";
 
 export default {
     'uppercase-literal-constant': UPPERCASE_LITERAL_CONSTANT,
     'method-naming': METHOD_NAMING,
+    'class-naming': CLASS_NAMING
 };
+
